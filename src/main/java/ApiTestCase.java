3bc21a2b62365a19235647fcff9a501521256352
Alex Dashkoff <alexey2109@gmail.com>
2017-08-01 15:05:40 -0700

    Changed api user

--- src/main/java/ApiTestCase.java
+++ src/main/java/ApiTestCase.java
@@ -7,7 +7,7 @@ public class ApiTestCase {
     @Test
     public void testUserInfoResponse() {
         RestTemplate restTemplate = new RestTemplate();
-        User user = restTemplate.getForObject("https://api.github.com/users/maxkolotilkin", User.class);
+        User user = restTemplate.getForObject("https://api.github.com/users/max2109", User.class);
         System.out.println(user);
 
 
