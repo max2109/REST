85180f4ed5cb97d26dd2448eb541e9153aeeecfc
Alex Dashkoff <alexey2109@gmail.com>
2017-08-01 15:24:13 -0700

    Add Assert

--- src/main/java/ApiTestCase.java
+++ src/main/java/ApiTestCase.java
@@ -1,15 +1,15 @@
 import models.User;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
-
+import static org.junit.Assert.*;
 public class ApiTestCase {
 
     @Test
     public void testUserInfoResponse() {
         RestTemplate restTemplate = new RestTemplate();
-        User user = restTemplate.getForObject("https://api.github.com/users/max2109", User.class);
-        System.out.println(user);
-
+        User remoteUser = restTemplate.getForObject("https://api.github.com/users/max2109", User.class);
+        assertTrue(remoteUser.getHtml_url().contains("https://github.com/max2109"));
+        //System.out.println(remoteUser);
 
     }
 
